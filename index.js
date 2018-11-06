a0220fc0a0b100ff98bd75c6bead31379f97d073
tstrat <tstrat@email.arizona.edu>
2018-11-06 11:11:47 -0700

    Added functions for die rolling

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // 3DR
+
+const dieRoller = (numberOfRolls, sides, staticModifier = 0) => {
+    let arr = [];
+    for (let i = 0; i < numberOfRolls; i++) {
+        arr.push(Math.floor(Math.random() * sides + 1));
+    }
+    let total = arr.reduce((t, n) => n + t, 0) + staticModifier;
+    return [arr, staticModifier, total ];
+}
+
+const d20Rolls = (staticModifier = 0) => {
+    const first = dieRoller(1, 20, staticModifier);
+    const second = dieRoller(1, 20, staticModifier);
+    const rtn = [ first[0].concat(second[0]), staticModifier, [first[2], second[2]] ];
+    return rtn;
+}
+
+const d20adv = (staticModifier = 0) => {
+    const rolls = d20Rolls(staticModifier);
+    return [ Math.max(rolls[0][0], rolls[0][1]), staticModifier, Math.max(rolls[0][0], rolls[0][1])];
+}
+
+const d20disAdv = (staticModifier = 0) => {
+    const rolls = d20Rolls(staticModifier);
+    return [ Math.min(rolls[0][0], rolls[0][1]), staticModifier, Math.min(rolls[0][0], rolls[0][1])];
+}
+const crit = (numberOfRolls, sides, staticModifier = 0) => {
+    const first = dieRoller(numberOfRolls, sides, staticModifier);
+    const second = dieRoller(numberOfRolls, sides, staticModifier);
+    total = first[0].reduce((t, n) => n + t, 0) + second[0].reduce((t, n) => n + t, 0) + staticModifier;
+    return [ first[0].concat(second[0]), staticModifier, total ];
+}
+
+const simpleRoll = (sides = 20) => {
+    return Math.floor(Math.random() * sides + 1);
+}
+
+const dieRollString = (numberOfRolls, sides, staticModifier) => {
+    let str = '';
+    
+    const roll = dieRoller(numberOfRolls, sides, staticModifier);
+    roll;
+    str += `( ${roll[0].join(', ')} ) + ${roll[1]} = ${roll[2]}`;
+    return str;
+}
+
+// TESTING
+
+let roll = dieRollString(2,20,11);
+console.log(roll);
+
+let critical = crit(2, 6, 5);
+critical;
+
+let adv = d20adv();
+console.log(adv);
+
+let disAdv = d20disAdv();
+console.log(disAdv);
